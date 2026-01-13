cd2735921b62b1368a3892002b112fdf5440b197
chouhanpradeep720-ai <chouhanpradeep720@gmail.com>
2026-01-13 17:50:03 +0530

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,7 +1,7 @@
 #!/user/bin/env groovy
 def call(String imagename) {
   withCredentials([usernamePassword(credentialsId: 'docker-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-                        sh "docker build -t $imagename."
+                        sh "docker build -t $imagename ."
                         sh 'echo $PASS | docker login -u $USER --password-stdin'
                         sh "docker push $imagename"
                     }
