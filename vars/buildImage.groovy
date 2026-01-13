1d2d3327c52a0269190339782244cf257a296682
chouhanpradeep720-ai <chouhanpradeep720@gmail.com>
2026-01-13 17:43:25 +0530

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,8 +1,8 @@
 #!/user/bin/env groovy
-def call() {
+def call(String imagename) {
   withCredentials([usernamePassword(credentialsId: 'docker-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-                        sh 'docker build -t pradeepchouhan115/docker.repo:jma-2.4 .'
+                        sh "docker build -t $imagename."
                         sh 'echo $PASS | docker login -u $USER --password-stdin'
-                        sh 'docker push pradeepchouhan115/docker.repo:jma-2.4'
+                        sh "docker push $imagename"
                     }
 }
